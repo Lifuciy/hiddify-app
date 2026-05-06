e69319633a4f0f7c5ec37e1f2c1b8a3bf6eadc5e
root <root@cqbslqodaj.local>
2026-05-06 17:24:12 +0000

    Add Windows launcher scripts

---
+++ ci/windows/run_start.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal EnableExtensions
+cd /d "%~dp0"
+
+set "EXE="
+if exist "PXY.exe" set "EXE=PXY.exe"
+if not defined EXE if exist "Hiddify.exe" set "EXE=Hiddify.exe"
+
+if not defined EXE (
+  echo [PXY] ERROR: cannot find PXY.exe or Hiddify.exe in "%CD%"
+  dir /b *.exe
+  pause
+  exit /b 1
+)
+
+echo [PXY] Starting: "%EXE%"
+start "" "%~dp0%EXE%" --disable-impeller --enable-software-rendering
