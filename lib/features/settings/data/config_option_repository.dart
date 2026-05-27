191fe749d003471969b11947b77e366e79726dd0
root <root@cqbslqodaj.local>
2026-05-27 18:56:34 +0000

    Revert experimental PXY TUN stability changes

--- lib/features/settings/data/config_option_repository.dart
+++ lib/features/settings/data/config_option_repository.dart
@@ -66,7 +66,7 @@ abstract class ConfigOptions {
 
   static final remoteDnsDomainStrategy = PreferencesNotifier.create<DomainStrategy, String>(
     "remote-dns-domain-strategy",
-    DomainStrategy.ipv4Only,
+    DomainStrategy.auto,
     mapFrom: (value) => DomainStrategy.values.firstWhere((e) => e.key == value),
     mapTo: (value) => value.key,
   );
@@ -91,7 +91,7 @@ abstract class ConfigOptions {
 
   static final directDnsDomainStrategy = PreferencesNotifier.create<DomainStrategy, String>(
     "direct-dns-domain-strategy",
-    DomainStrategy.ipv4Only,
+    DomainStrategy.auto,
     mapFrom: (value) => DomainStrategy.values.firstWhere((e) => e.key == value),
     mapTo: (value) => value.key,
   );
@@ -125,7 +125,7 @@ abstract class ConfigOptions {
     mapTo: (value) => value.name,
   );
 
-  static final mtu = PreferencesNotifier.create<int, int>("mtu", 1400);
+  static final mtu = PreferencesNotifier.create<int, int>("mtu", 9000);
 
   static final strictRoute = PreferencesNotifier.create<bool, bool>("strict-route", true);
 
@@ -350,8 +350,6 @@ abstract class ConfigOptions {
 
   static final singboxConfigOptions = Provider<SingboxConfigOption>((ref) {
     final selectedRegion = ref.watch(region);
-
-
     final rules = switch (selectedRegion) {
       Region.ru => [
           const SingboxRule(
@@ -364,7 +362,6 @@ abstract class ConfigOptions {
     };
 
     final mode = ref.watch(serviceMode);
-    final isTunMode = mode == ServiceMode.tun || mode == ServiceMode.tunService;
     // final reg = ref.watch(Preferences.region.notifier).raw();
 
     return SingboxConfigOption(
@@ -375,17 +372,17 @@ abstract class ConfigOptions {
       executeConfigAsIs: false,
       logLevel: ref.watch(logLevel),
       resolveDestination: ref.watch(resolveDestination),
-      ipv6Mode: isTunMode ? IPv6Mode.disable : ref.watch(ipv6Mode),
-      remoteDnsAddress: isTunMode ? "tcp://8.8.8.8" : ref.watch(remoteDnsAddress),
-      remoteDnsDomainStrategy: isTunMode ? DomainStrategy.ipv4Only : ref.watch(remoteDnsDomainStrategy),
-      directDnsAddress: isTunMode ? "tcp://1.1.1.1" : ref.watch(directDnsAddress),
-      directDnsDomainStrategy: isTunMode ? DomainStrategy.ipv4Only : ref.watch(directDnsDomainStrategy),
+      ipv6Mode: ref.watch(ipv6Mode),
+      remoteDnsAddress: ref.watch(remoteDnsAddress),
+      remoteDnsDomainStrategy: ref.watch(remoteDnsDomainStrategy),
+      directDnsAddress: ref.watch(directDnsAddress),
+      directDnsDomainStrategy: ref.watch(directDnsDomainStrategy),
       mixedPort: ref.watch(mixedPort),
       tproxyPort: ref.watch(tproxyPort),
       directPort: ref.watch(directPort),
       redirectPort: ref.watch(redirectPort),
       tunImplementation: ref.watch(tunImplementation),
-      mtu: isTunMode ? 1400 : ref.watch(mtu),
+      mtu: ref.watch(mtu),
       strictRoute: ref.watch(strictRoute),
       connectionTestUrl: ref.watch(connectionTestUrl),
       urlTestInterval: ref.watch(urlTestInterval),
@@ -396,7 +393,7 @@ abstract class ConfigOptions {
       setSystemProxy: mode == ServiceMode.systemProxy,
       bypassLan: ref.watch(bypassLan),
       allowConnectionFromLan: ref.watch(allowConnectionFromLan),
-      enableFakeDns: isTunMode ? false : ref.watch(enableFakeDns),
+      enableFakeDns: ref.watch(enableFakeDns),
       // enableDnsRouting: ref.watch(enableDnsRouting),
       independentDnsCache: ref.watch(independentDnsCache),
       mux: SingboxMuxOption(
